4c0e79420146136e594c691d6ab38971c255c9cf
Jack Parsons <jackpar1113@gmail.com>
2022-09-28 21:15:40 -0600

    complete contains duplicates

---
+++ contains_duplicate.py
@@ -0,0 +1,8 @@
+def containsDuplicate(nums):
+    count = {}
+    for num in nums:
+        count[num] = count.get(num, 0) + 1
+        if count[num] > 1:
+            return True
+    return False
+
