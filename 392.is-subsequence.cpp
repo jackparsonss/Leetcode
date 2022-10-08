e075ef72fcd8ab3680cf32d188c2212762979278
Jack Parsons <jackpar1113@gmail.com>
2022-10-07 19:51:11 -0600

    complete is subsequence

---
+++ 392.is-subsequence.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=392 lang=cpp
+ *
+ * [392] Is Subsequence
+ */
+#include<string>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+
+        while(i != s.size() && j != t.size()) {
+            if(s[i] == t[j]) {
+                i++; j++;
+            } else {
+                j++;
+            }
+        }
+
+        if(i == s.size()) {
+            return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
