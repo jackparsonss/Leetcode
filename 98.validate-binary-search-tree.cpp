3d4c1db80db9cf39d95dfe68bad3778b247fb074
Jack Parsons <jackpar1113@gmail.com>
2022-10-11 20:49:40 -0600

    complete 'validate binary search tree'

---
+++ 98.validate-binary-search-tree.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#include<limits.h>
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, LONG_MIN, LONG_MAX);
+    }
+
+    bool isValid(TreeNode* root, long min, long max) {
+        if(root == nullptr) {
+            return true;
+        }
+
+        if(root->val >= max || root->val <= min) {
+            return false;
+        }
+
+        return isValid(root->left, min, root->val) && isValid(root->right, root->val, max);
+    }
+};
+// @lc code=end
+
