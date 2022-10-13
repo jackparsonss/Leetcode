5dfaa1455417b762800eb4c34aac841a42ebcf4d
Jack Parsons <jackpar1113@gmail.com>
2022-10-12 20:48:49 -0600

    complete 'long repeating character replacement'

--- 424.longest-repeating-character-replacement.cpp
+++ 424.longest-repeating-character-replacement.cpp
@@ -6,11 +6,23 @@
 
 // @lc code=start
 #include<string>
+#include<vector>
 using namespace std;
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        vector<int> arr(128);
+        int start = 0, largest = 0;
+
+        for (int i = 0; i < s.size(); i++){
+            largest = max(largest, ++arr[s[i]]);
+            if(i - start + 1 - largest > k) {
+                arr[s[start++]]--;
+            }
+        }
         
+
+        return s.size() - start;
     }
 };
 // @lc code=end
