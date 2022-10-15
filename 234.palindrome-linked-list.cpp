87aa82aaf3ac96f3626d36ae24db2d85d74e300e
Jack Parsons <jackpar1113@gmail.com>
2022-10-15 17:35:42 -0600

    complete 'palindrome linked list'

---
+++ 234.palindrome-linked-list.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=234 lang=cpp
+ *
+ * [234] Palindrome Linked List
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include<stack>
+using namespace std;
+class Solution {
+public:
+    ListNode* reverse_l(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while(curr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        ListNode* half = head;
+        ListNode* fast = head;
+        while(fast && fast->next) {
+            half = half->next;
+            fast = fast->next;
+        }
+
+        ListNode* first_half = head;
+        ListNode* second_half = reverse_l(half);
+
+        while(second_half) {
+            if(first_half->val != second_half->val) {
+                return false;
+            }
+            first_half = first_half->next;
+            second_half = second_half->next;
+        }
+        return true;
+    }
+
+};
+// @lc code=end
+
