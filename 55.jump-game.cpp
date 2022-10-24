96d1a639d2f0bb2a30545e980f1ada0f2514282e
Jack Parsons <jackpar1113@gmail.com>
2022-10-23 22:27:14 -0600

    complete 'jump game'

---
+++ 55.jump-game.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=55 lang=cpp
+ *
+ * [55] Jump Game
+ */
+#include<vector>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int reachable = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (i > reachable) {
+                return false;
+            }
+            reachable = max(reachable, i + nums[i]);
+            if (reachable >= n - 1) {
+                break;
+            }
+        }
+        
+        return true;
+    }
+};
+// @lc code=end
+
